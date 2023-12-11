e3b3d81def28db75146fb75f1c2b5ef953215066
iAmRishijha <rishijha238@gmail.com>
2023-12-11 19:28:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Add event listeners to update the active class when scrolling
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
